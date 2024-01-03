f3f66df55a69b6bbb0cc94318a65f874fd99f7ba
PREDATOR <prateek772492@gmail.com>
2024-01-03 10:54:17 +0530

    Triggers Introduction

---
+++ Advanced Mysql Queries/triggers.sql
@@ -0,0 +1,66 @@
+/*
+A MySQL trigger is a set of actions available in the form of a stored program. 
+These set of actions are invoked automatically when certain events occur.
+
+- CREATION
+CREATE TRIGGER trigger_name
+TRIGGER TYPE - INSERT, UPDATE OR DELETE
+EXECUTE TYPE - BEFORE OR AFTER
+ON table_name FOR EACH ROW
+BEGIN
+	TRIGGER LOGIC
+END
+
+- DELETION
+DROP TRIGGER IF EXISTS schema_name.trigger_name
+
+if a table is deleted/dropped from database, all the triggers associated with it also get deleted. 
+
+Row level Trigger:
+Invoked for every row inserted, updated or deleted in a table. 100 rows, 100 times trigger activation
+
+Statement Level Trigger:
+Invoked once for each action and it occurs just once, no matter how many rows are inserted, updated or deleted.alter
+
+Before: trigger invoked before any action is performed on table row.
+After: trigger invokd  after the action is performed on each row. 
+
+Before Insert, After Insert
+Before Update, After Update
+Before Delete, After Delete
+
+
+*/
+
+-- BEFOR INSERT
+DELIMITER //
+CREATE TRIGGER OrderQtyCheck
+BEFORE INSERT
+ON orders FOR EACH ROW
+BEGIN
+	IF NEW.orderQty < 0 THEN
+		SET NEW.orderQty = 0;
+    END IF;
+END; //
+DELIMITER ;
+
+
+-- AFTER INSERT
+DELIMITER //
+CREATE TRIGGER LogNewOrderInsert
+AFTER INSERT
+ON Orders FOR EACH ROW
+INSERT INTO Audits VALUES('AFTER', 'A new order is inserted', 'INSERT');
+DELIMITER ;
+
+
+-- AFTER DELETE
+DELIMITER //
+CREATE TRIGGER AfterDeleteOrder
+AFTER DELETE
+ON Orders FOR EACH ROW
+INSERT INTO Audits
+VALUES ('AFTER', CONCAT('Order', OLD.OrderID, ' was deleted at ', CURRENT_TIME(), ' on ', CURRENT_DATE()), 'DELETE');
+DELIMITER ;
+
+
